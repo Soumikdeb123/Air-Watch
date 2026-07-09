3cd1e6f2f79dcce2774cd4995a87ecfa8722d80d
Soumikdeb-wq <152465385+Soumikdeb123@users.noreply.github.com>
2026-07-09 10:09:34 +0930

    Set up Spring Boot project and added EPA datasets

---
+++ src/main/java/com/airwatch/AirWatchApplication.java
@@ -0,0 +1,11 @@
+package com.airwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirWatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirWatchApplication.class, args);
+    }
+}
